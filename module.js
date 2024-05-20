3880b21017da37f01489907b9a3b98909f8d03c9
Pinki Kumari <166779907+PinkiKumari16@users.noreply.github.com>
2024-05-20 11:31:32 +0530

    Add files via upload

---
+++ module.js
@@ -0,0 +1,14 @@
+// const message = "Hello Pinki";
+
+// function add(a,b){
+//     return a+b ;
+// }
+
+
+// export default function (){
+//     console.log('hello ppppppppppppppppp  ');
+// }
+
+
+// export {message, add};
+
